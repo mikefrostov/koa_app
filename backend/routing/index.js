eeca7a6f40b697c71893857237b27838031168af
mikefrostov <m.e.morozov1@gmail.com>
2020-05-10 15:59:49 +0300

    fix crud for new db

--- backend/routing/index.js
+++ backend/routing/index.js
@@ -9,11 +9,8 @@ const router = Router()
 //  get posts
 router.get('/posts/:listid', async ctx => {
         listid = ctx.params.listid;
-        var queryConfig = {
-            text: 'SELECT * FROM $1 ORDER BY id',
-            values: [listid]
-        };
-        result = await database.query(queryConfig).then(c => c.rows);
+        var queryConfig = `SELECT * FROM ${listid} ORDER BY id;`
+	result = await database.query(queryConfig).then(c => c.rows);
         ctx.status = 200;
         ctx.body = result;
 });
@@ -21,19 +18,13 @@ router.get('/posts/:listid', async ctx => {
 // create post
 router.post('/posts/:listid', koaBody(), async ctx => {
         post = JSON.parse(JSON.stringify(ctx.request.body.post));
-        listid = ctx.params.listid;
-        var queryConfig = {
-            text: 'INSERT INTO $1 ( body) VALUES ( $2) RETURNING id',
-            values: [listid, post]
-        };
-        result1 = await database.query(queryConfig).then(c => c.rows[0].id);
-	var queryConfig2 = {
-	    text: 'SELECT * FROM $1 WHERE $1.id = $2;',   // a question for myself: why select * ???
-	    values: [listid, result1]
-	};
-        result = await database.query(queryConfig2).then(c => c.rows);
-        ctx.status = 200;
-        ctx.body = result;
+	listid = ctx.params.listid;
+        var queryConfig = `INSERT INTO ${listid} ( body ) VALUES ( '${post}' ) RETURNING id;`
+	result1 = await database.query(queryConfig).then(c => c.rows[0].id);
+	var queryConfig2 = `SELECT * FROM ${listid} WHERE ${listid}.id = ${result1};`
+	result = await database.query(queryConfig2).then(c => c.rows);
+	ctx.status = 200;
+	ctx.body = result1;
 });
 
 // update post
@@ -41,16 +32,8 @@ router.put('/posts/:listid', koaBody(), async ctx => {
         listid = ctx.params.listid;
         postid = JSON.stringify(ctx.request.body.id);
         post = JSON.parse(JSON.stringify(ctx.request.body.post));
-	var queryConfig = {
-            text: 'UPDATE $3 SET body = $1 WHERE $3.id = $2;',
-            values: [post, postid, listid]
-        };
-
-        var returnListPosts = {
-                text: 'SELECT * FROM $1 ORDER BY id;',
-                values: [listid]
-            };
-
+        var queryConfig = `UPDATE ${listid} SET body = '${post}' WHERE ${listid}.id = '${postid}';` 
+        var returnListPosts = `SELECT * FROM ${listid} ORDER BY id;`
         result1 = await database.query(queryConfig);
         result = await database.query(returnListPosts).then(c => c.rows);
         ctx.status = 200;
@@ -61,18 +44,10 @@ router.put('/posts/:listid', koaBody(), async ctx => {
 router.del('/posts/:listid', koaBody({ strict: false }), async ctx => {
       listid = ctx.params.listid;
       postid = JSON.stringify(ctx.request.body.id);
-      var queryConfig1 = {
-      text: 'DELETE FROM $2 WHERE $2.id = $1;',
-      values: [postid, listid]
-  };
-        result1 = await database.query(queryConfig1);
-
-      var queryConfig = {
-      text: 'SELECT * FROM $1 ORDER BY id;',
-      values: [listid]
-  };
-        result = await database.query(queryConfig).then(c => c.rows);
-        
+        var queryConfig1 = `DELETE FROM ${listid} WHERE ${listid}.id = '${postid}';`
+	result1 = await database.query(queryConfig1);
+        var queryConfig = `SELECT * FROM ${listid} ORDER BY id;`
+	result = await database.query(queryConfig).then(c => c.rows);
 	ctx.status = 200;
         ctx.body = result;
 });
@@ -81,10 +56,6 @@ router.del('/posts/:listid', koaBody({ strict: false }), async ctx => {
 router.post('/lists/:listid', koaBody({ strict: false }), async ctx => {
 	listid = ctx.params.listid;
 	
-	//var queryConfig = {
-        //text: 'CREATE TABLE $1 (id SERIAL NOT NULL PRIMARY KEY, body TEXT NOT NULL, createdat TIMESTAMPTZ NOT NULL DEFAULT NOW());',
-        //values: [listid] 
-	//};
         var queryConfig = `CREATE TABLE ${listid} (id SERIAL NOT NULL PRIMARY KEY, body TEXT NOT NULL, createdat TIMESTAMPTZ NOT NULL DEFAULT NOW());`
 //	result = await database.query(queryConfig).then(c => c.rows);
         result = await database.query(queryConfig)

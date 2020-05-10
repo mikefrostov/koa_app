2ba363370243f4893a6d179b7df0e379ff90b946
mikefrostov <m.e.morozov1@gmail.com>
2020-05-10 23:40:03 +0300

    fix hash string

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,52 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'reactstrap'
 import Placeholder from './Placeholder';
 import { Button } from 'react-bootstrap';
-// Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
-
-//function fetch() {
-//  return new Promise(resolve => setTimeout(() => resolve(42), 1000));
-//}
-
-//submitFormAdd = e => {
-/*    fetch('http://192.168.1.7:3002/posts/' + this.state.listid, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        post: this.state.post
-      })
-    })
-      .then(response => response.json())
-      .then(item => {
-        if(Array.isArray(item)) {
-         this.props.addItemToState(item[0])
-         this.props.toggle()
-        } else {
-          console.log('failure')
-        }
-      })
-      .catch(err => console.log(err))
-  }*/
 
 
 function fetchAPI(param) {
-  // param is a highlighted word from the user before it clicked the button
-//  return fetch("https://api.com/?param=" + param);
         var listid = "default"
-	listid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+	listid = Math.random().toString(36).replace(/[^a-z]+/g, '') + Math.random().toString(36).substring(2, 36);
 	console.log("generated listid = " + listid) 
       return fetch('http://192.168.1.7:3002/lists/' + listid, {
       method: 'POST',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json'
-      }//,
-      //body: JSON.stringify({
-      //  post: this.state.post
-      //})
+      }
     })
       .then(response => response.json())
       .then(item => {
@@ -65,8 +31,7 @@ function fetchAPI(param) {
 
 
 class Home extends Component {
-    state = { result : null };
-    
+  state = { result : null };  
   toggleButtonState = () => {
     let selectedWord = window.getSelection().toString();
     fetchAPI(selectedWord).then(result => {

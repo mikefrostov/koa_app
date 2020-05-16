ef4c2a4a82d94c72a278d705df4993cc8f64af61
mikefrostov <m.e.morozov1@gmail.com>
2020-05-16 14:37:15 +0300

    fix link home-page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -70,7 +70,7 @@ class Home extends Component {
     
 <button class="btn btn-lg btn-secondary" onClick={this.toggleButtonState}>Generate</button>
 
-<div style={{padding: "15px", fontSize: "25px",color: "green"}}><a href = "http://192.168.1.7:4001/{this.state.listid}">{this.state.listid} </a> </div>
+<div style={{padding: "15px", fontSize: "25px",color: "green"}}><a href={'http://192.168.1.7:4001/' + this.state.listid}> {this.state.listid} </a> </div>
 
 </body>
     );

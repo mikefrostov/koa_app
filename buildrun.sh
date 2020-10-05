6bc71b91494f4b30a8e3af7eab3e62ca014008b2
mikefrostov <m.e.morozov1@gmail.com>
2020-10-05 17:46:18 +0000

    switch ct startup order

--- buildrun.sh
+++ buildrun.sh
@@ -9,6 +9,6 @@ npm install -y
 npm run-script build
 sudo docker build -t front:1 .
 cd ../
-sudo ./backend/postgrescontainer.sh
 sudo docker run -d -p 4001:4001 -t front:1
 sudo docker run -d -p 4002:4002 -t back:1
+sudo ./backend/postgrescontainer.sh

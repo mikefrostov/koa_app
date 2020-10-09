b3b36fa9f3b6b7c6f9b56e3602c23c1fb9295997
mikefrostov <m.e.morozov1@gmail.com>
2020-10-09 06:41:48 +0000

    rearrange rm script

--- rmapp.sh
+++ rmapp.sh
@@ -1,11 +1,21 @@
 #!/bin/bash 
 set -e 
 
+#remove backend ct and image
+echo 'removing backend ct and image: '
 sudo docker stop $(sudo docker ps -q --filter ancestor=back:1 )
-sudo docker stop $(sudo docker ps -q --filter ancestor=front:1 )
-sudo docker stop $(sudo docker ps -q --filter ancestor=postgres )
 sudo docker rm $(sudo docker ps --all -q --filter ancestor=back:1 )
-sudo docker rm $(sudo docker ps --all -q --filter ancestor=front:1 )
-sudo docker rm $(sudo docker ps --all -q --filter ancestor=postgres )
 sudo docker image rm $( sudo docker images -q --filter reference='back:1' )
+
+echo 'removing frontend ct and image: '
+#remove front ct and image
+sudo docker stop $(sudo docker ps -q --filter ancestor=front:1 )
+sudo docker rm $(sudo docker ps --all -q --filter ancestor=front:1 )
 sudo docker image rm $( sudo docker images -q --filter reference='front:1' )
+
+
+echo 'removing postgres ct'
+sudo docker stop $(sudo docker ps -q --filter ancestor=postgres )
+sudo docker rm $(sudo docker ps --all -q --filter ancestor=postgres )
+
+

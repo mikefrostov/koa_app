42d7f9020a5a9c371b22bd8b97fa82dabe6c6a46
root <root@master>
2020-10-09 05:58:07 +0000

    rework delete script

--- rmapp.sh
+++ rmapp.sh
@@ -3,5 +3,9 @@ set -e
 
 sudo docker stop $(sudo docker ps -q --filter ancestor=back:1 )
 sudo docker stop $(sudo docker ps -q --filter ancestor=front:1 )
+sudo docker stop $(sudo docker ps -q --filter ancestor=postgres )
 sudo docker rm $(sudo docker ps --all -q --filter ancestor=back:1 )
 sudo docker rm $(sudo docker ps --all -q --filter ancestor=front:1 )
+sudo docker rm $(sudo docker ps --all -q --filter ancestor=postgres )
+sudo docker images rm $( sudo docker images -q --filter reference='back:1' )
+sudo docker images rm $( sudo docker images -q --filter reference='front:1' )

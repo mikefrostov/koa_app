8a4cc83b9020b2c084120054e78b2177b3bab615
mikefrostov <m.e.morozov1@gmail.com>
2020-10-10 17:31:37 +0000

    post fix

--- rmapp.sh
+++ rmapp.sh
@@ -4,13 +4,13 @@ set -e
 #remove backend ct and image
 echo 'removing backend ct and image: '
 sudo docker stop $(sudo docker ps -q --filter ancestor=back:1 )
-sudo docker rm $(sudo docker ps --all -q --filter ancestor=back:1 )
+#sudo docker rm $(sudo docker ps --all -q --filter ancestor=back:1 )
 sudo docker image rm $( sudo docker images -q --filter reference='back:1' )
 
 echo 'removing frontend ct and image: '
 #remove front ct and image
 sudo docker stop $(sudo docker ps -q --filter ancestor=front:1 )
-sudo docker rm $(sudo docker ps --all -q --filter ancestor=front:1 )
+#sudo docker rm $(sudo docker ps --all -q --filter ancestor=front:1 )
 sudo docker image rm $( sudo docker images -q --filter reference='front:1' )
 
 
